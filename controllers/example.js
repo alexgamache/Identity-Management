608aabc3be9e231bf004cdf14ec5cb214ac55a3d
Ubuntu <ubuntu@ip-172-26-2-174.us-east-2.compute.internal>
2020-09-22 00:36:46 +0000

    finished example endpoint

--- controllers/example.js
+++ controllers/example.js
@@ -1,3 +1,5 @@
+const exampleService = require('../services/example-service')
+//Do web stuff here (interact with res/req import and excecute services)
 exports.test = function (req, res){
-    res.send("Hello world");
+    res.send(exampleService.greeting());
 }
---
+++ services/example-service.js
@@ -0,0 +1,4 @@
+exports.greeting = function(){
+    //Do "real" stuff here in a service (db calls, filesystem stuff, etc)
+    return "hello world"
+}
