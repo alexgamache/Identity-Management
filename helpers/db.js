e87d2a5fce8dbbb186c4fb33ca1e4d7276e7a840
dldonofrio <dandono97@gmail.com>
2020-09-23 18:34:56 -0400

    recovered helpers folder in dev branch

---
+++ helpers/db.js
@@ -0,0 +1,11 @@
+//DB connector
+const mysql = require('serverless-mysql')({
+  config: {
+	  host     : process.env.ENDPOINT,
+	  database : process.env.DATABASE,
+	  user     : process.env.USERNAME,
+	  password : process.env.PASSWORD
+  }
+})
+
+exports.mysql = mysql
