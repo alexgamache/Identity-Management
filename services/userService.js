331eb29b5eb6aefbc9d7c325b8178ca970621aaf
Ubuntu <ubuntu@ip-172-26-2-174.us-east-2.compute.internal>
2020-09-23 14:51:17 +0000

    Fixed typo

--- services/userService.js
+++ services/userService.js
@@ -7,7 +7,7 @@ exports.register = async function(userObj){
 	await mysql.query("insert into user (fname, lname, email, is_admin) Values(?,?,?,?)", [userObj.fname, userObj.lname, userObj.email, 0])
 	const dbObj = await mysql.query("select * from user where email = ?", [userObj.email])
 	await mysql.end()
-	const token = jwt.sign(dbObj[0], process.env.SECRET);
+	const token = await jwt.sign(dbObj[0], process.env.SECRET);
 	return {
 	    status: "Good",
 	    message: "user registered successfully",
