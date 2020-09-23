8830083c2f6b6f9085e5556c9f54f2e8af493656
dldonofrio <dandono97@gmail.com>
2020-09-23 18:24:43 -0400

    more changes

---
+++ controllers/userController.js
@@ -0,0 +1,72 @@
+//userController
+const userService = require('../services/userService')
+
+exports.register = async function(req, res){
+    try{
+		const user = req.body.user
+		if(!user){
+			res.send(JSON.stringify({
+				status: 400,
+				message: "No user object specified"
+			}))
+		}else{
+			const registration = await userService.register(user)
+			if(registration.status === "Good"){
+				res.send(JSON.stringify({
+					status: 200,
+					message: registration.message,
+					data: registration.token
+				}))
+			}else{
+			res.send(JSON.stringify({
+				status: 500,
+				message: "There was an error registering this user"
+			}))
+			}
+		}
+    }
+    catch(err){
+		res.send(JSON.stringify({
+			status: 400,
+			message: err.message
+		}))
+    }
+}
+
+
+exports.login = async function(req, res){
+	
+}
+
+
+exports.manageStatus = async function(req, res){
+	try{
+		const task = req.body.task
+		const user = req.body.id
+		if(!task || !id){
+			res.send(JSON.stringify({
+				status: 400,
+				message: "This endpoint requires both task and id. One of those were missing check data for what was received",
+				data: req.body
+			}))
+		}
+		const result = await userService.lockAccount(user, task)
+		if(result.status === "Done"){
+			res.send(JSON.stringify({
+				status: 200,
+				message: result.message
+			}))
+		}else{
+			res.send(JSON.stringify({
+				status: 400,
+				message: result.message
+			}))
+		}
+	}
+	catch(err){
+		res.send(JSON.stringify({
+			status: 400,
+			message: err.message
+		}))
+	}
+}
---
+++ services/userService.js
@@ -0,0 +1,76 @@
+//userService
+const sendmail = require('../helpers/sendMail').sendMail
+var jwt = require('jsonwebtoken')
+
+exports.register = async function(userObj){
+    try{
+		const mysql = require('../helpers/db').mysql
+		await mysql.query("insert into user (fname, lname, email, is_admin) Values(?,?,?,?)", [userObj.fname, userObj.lname, userObj.email, 0])
+		const dbObj = await mysql.query("select * from user where email = ?", [userObj.email])
+		await mysql.end()
+		const token = await jwt.sign(dbObj[0], process.env.SECRET);
+		await sendmail(userObj.email)
+		return {
+			status: "Good",
+			message: "user registered successfully",
+			token: token
+		}
+    }
+    catch(err){
+		return {
+			status: "bad",
+			message: err.message,
+			token: null
+		}
+    }
+}
+
+exports.login = async function(userObj){
+	try{
+		const mysql = require('../helpers/db').mysql
+		var email = userObj.email;
+		var password = userObj.password;
+		mysql.query('SELECT * FROM user WHERE email =?' [email], async function (error, results, fields) {
+			if (error) {
+				return {
+					status: "Bad",
+					message: "Code 400, bad email lookup",
+					token: token
+				} else {
+					if(results.length > 0) {
+						if (passwor)
+					}
+				}
+			}
+		})
+	}
+ 
+}
+
+
+exports.lockAccount = async function(id, task){
+	try{
+		const mysql = require('../helpers/db').mysql
+		if (task === "lock"){
+			await mysql.query('update user set isLocked = ? where id = ?', [1, id])
+		}else if(task === "unlock"){
+			await mysql.query('update user set isLocked = ? where id = ?', [0, id])
+		}else{
+			return {
+				status :"error",
+				message: "task not recognized"
+			}
+		}
+		await mysql.end()
+		return {
+			status: "Done",
+			message: `user account successfully ${task}ed`
+		}
+	}
+	catch(err){
+		return {
+			status: "error",
+			message: err.message
+		}
+	}
+}
