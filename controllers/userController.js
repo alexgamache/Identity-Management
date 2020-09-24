d0fa2654f3be8bfad8f6bb3a2b226a60d7ce5294
Jonathan <jmukes@oakland.edu>
2020-09-23 20:43:44 -0400

    various api fixes

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,31 +5,31 @@ exports.register = async function(req, res){
     try{
 		const user = req.body.user
 		if(!user){
-			res.send(JSON.stringify({
+			res.send({
 				status: 400,
 				message: "No user object specified"
-			}))
+			})
 		}else{
 			const registration = await userService.register(user)
 			if(registration.status === "Good"){
-				res.send(JSON.stringify({
+				res.send({
 					status: 200,
 					message: registration.message,
 					data: registration.token
-				}))
+				})
 			}else{
-			res.send(JSON.stringify({
+			res.send({
 				status: 500,
 				message: "There was an error registering this user"
-			}))
+			})
 			}
 		}
     }
     catch(err){
-		res.send(JSON.stringify({
+		res.send({
 			status: 400,
 			message: err.message
-		}))
+		})
     }
 }
 
@@ -38,29 +38,29 @@ exports.manageStatus = async function(req, res){
 		const task = req.body.task
 		const user = req.body.id
 		if(!task || !id){
-			res.send(JSON.stringify({
+			res.send({
 				status: 400,
 				message: "This endpoint requires both task and id. One of those were missing check data for what was received",
 				data: req.body
-			}))
+			})
 		}
 		const result = await userService.lockAccount(user, task)
 		if(result.status === "Done"){
-			res.send(JSON.stringify({
+			res.send({
 				status: 200,
 				message: result.message
-			}))
+			})
 		}else{
-			res.send(JSON.stringify({
+			res.send({
 				status: 400,
 				message: result.message
-			}))
+			})
 		}
 	}
 	catch(err){
-		res.send(JSON.stringify({
+		res.send({
 			status: 400,
 			message: err.message
-		}))
+		})
 	}
 }
--- services/userService.js
+++ services/userService.js
@@ -6,9 +6,17 @@ exports.register = async function(userObj){
     try{
 		const mysql = require('../helpers/db').mysql
 		await mysql.query("insert into user (fname, lname, email, is_admin) Values(?,?,?,?)", [userObj.fname, userObj.lname, userObj.email, 0])
+		const checkIfExists = await mysql.query("Select * from user where email = ?", [userObj.email])
+		if(checkIfExists.length){
+			 return {
+				 status: "bad",
+				 message: "User already exists",
+				 token: null
+			 }
+		}
 		const dbObj = await mysql.query("select * from user where email = ?", [userObj.email])
 		await mysql.end()
-		const token = await jwt.sign(dbObj[0], process.env.SECRET);
+		const token = await jwt.sign({user: dbObj[0]}, process.env.SECRET);
 		await sendmail(userObj.email)
 		return {
 			status: "Good",
