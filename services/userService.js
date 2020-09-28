9331bb50bcd3632f240ccbefcbb13efc9d93063c
flexerblex <flexerblex@gmail.com>
2020-09-28 14:38:55 -0400

    Added username field

--- services/userService.js
+++ services/userService.js
@@ -5,7 +5,7 @@ var jwt = require('jsonwebtoken')
 exports.register = async function(userObj){
     try{
 		const mysql = require('../helpers/db').mysql
-		await mysql.query("insert into user (fname, lname, email, is_admin) Values(?,?,?,?)", [userObj.fname, userObj.lname, userObj.email, 0])
+		await mysql.query("insert into user (fname, lname, email, is_admin, username) Values(?,?,?,?,?)", [userObj.fname, userObj.lname, userObj.email, 0, userObj.username])
 		const dbObj = await mysql.query("select * from user where email = ?", [userObj.email])
 		await mysql.end()
 		const token = await jwt.sign(dbObj[0], process.env.SECRET);
