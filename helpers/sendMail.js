37036b48c221fdc9959e71356b775bc35584c8a8
ChengPan2333 <60989982+ChengPan2333@users.noreply.github.com>
2020-09-30 15:40:43 -0400

    Email Confirmation

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,27 +1,27 @@
 //send email here
 //exports.sendMail = async function(email)
-
 const nodemailer = require('nodemailer');
-var toEmail = 'someone@yahoo.com';
+var toEmail = 'lpan@oakland.com';
 module.exports = Object.freeze({
   sendEmailNotification
 });
 
+//Use gmail service
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'xxxx@gmail.com',
-    pass: 'yyyy'
+    user: 'Lexyptest@gmail.com',
+    pass: 'Qw12er34'
   }
 });
 
 function sendEmailNotification(userName){
 
-  const mailOptions = {
-    from: 'xxxx@gmail.com',
+  const mailOptions = { //Email template
+    from: 'Lexyptest@gmail.com',
     to: toEmail,
-    subject: 'Email Verification',
-    text: `Thank you for registering our website!`
+    subject: 'Email Comfirmation',
+    text: 'Thank you for registering our website!'
   };
 
   transporter.sendMail(mailOptions, function(error, info){
