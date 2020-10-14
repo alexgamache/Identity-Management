3d1382612e53d089f4d6ebb1cb7aa81df8385555
flexerblex <flexerblex@gmail.com>
2020-10-14 16:56:22 -0400

    Removed Passport

--- index.js
+++ index.js
@@ -8,13 +8,10 @@ const port = 80
 const multer = require('multer');
 const fileUpload = require('express-fileupload');
 const auth = require("./helpers/token")
-const passport = require('passport');
-require('./passport-setup');
 
 //Import controllers here
 const example = require('./controllers/example')
 const user = require('./controllers/userController')
-const google = require('./controllers/oauthController')
 
 //add middleware here
 app.use(async function (req, res, next) {
@@ -37,14 +34,10 @@ app.use(async function (req, res, next) {
   }
 })
 
-
 app.use(bodyParser.json())
 app.use(fileUpload({
-  createParentPath: true
+    createParentPath: true
 }));
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 //link endpoints to controller functions here
 app.get('/', example.test)
@@ -52,12 +45,6 @@ app.post('/register', user.register)
 app.post('/manage', user.manageStatus)
 app.post('/upload', user.upload)
 app.post('/login', user.login)
-
-app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
-
-app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }), google.callback)
-
-app.get('/failed', google.fail)
 
 //listen on the url
 app.listen(port, () => {
